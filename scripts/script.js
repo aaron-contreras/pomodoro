5d67412b2ffdfd91d061261df138b27c2135df91
Aaron Contreras <aaronlcaq@gmail.com>
2020-05-03 10:36:56 -0500

    Change minute and second values to 'number' type

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,26 @@
 function showClock(clock) {
-  return clock.minutes + clock.separator + clock.seconds.padEnd(2, '0');
+  return clock.minutes.toString().padStart(2, '0') + clock.separator + clock.seconds.toString().padStart(2, '0');
 }
 
 function timerIsUp(clock) {
   return clock.minutes === 0 && clock.seconds === 0;
 }
 function startPomodoro() {
-  const time = prompt('How many minutes of focus?');
+  const time = +prompt('How many minutes of focus?');
   const clock = {
     minutes: time,
     separator: ':',
-    seconds: '0'
+    seconds: 0
   }
+  
   const countdown = setInterval(decreaseSecond, 1000);
   function decreaseMinute(clock) {
     clock.minutes--;
-    clock.seconds = '59';
+    clock.seconds = 59;
   }
 
   function decreaseSecond() {
-    clock.seconds = (clock.seconds - 1).toString();
+    clock.seconds--;
     if (clock.seconds < 0) {
       decreaseMinute(clock);
     }
@@ -28,7 +29,6 @@ function startPomodoro() {
       clearInterval(countdown);
     }
   }
-  
 }
 
 startPomodoro();
