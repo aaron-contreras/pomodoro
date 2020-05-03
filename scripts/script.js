85112b5dcfa9078704a36f72090d056d01bc0462
Aaron Contreras <aaronlcaq@gmail.com>
2020-05-03 13:33:01 -0500

    Add progress bar animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,24 @@
+function updateSessions(sessionNum) {
+  session.textContent = sessionNum;
+  if (sessions % 4 !== 0) {
+    startBreak();
+  } else {
+    startLongBreak();
+  }
+}
+
 function showClock(clock) {
 
-  const timeRemaining = clock.minutes.toString().padStart(2, '0') + clock.separator + clock.seconds.toString().padStart(2, '0');
-  clockHeading.textContent = timeRemaining;
+  const timeRemaining = (
+    clock.minutes.toString().padStart(2, '0') +
+    clock.separator +
+    clock.seconds.toString().padStart(2, '0')
+  );
+  
+  const relevantClock = clocks.find(elem => {
+    return elem.classList.contains(clock.type);
+  });
+  relevantClock.textContent = timeRemaining;
   return timeRemaining;
 }
 
@@ -11,24 +28,35 @@ function timerIsUp(clock) {
 
 function pausePomodoro() {
   state = 'paused';
-  clearInterval(intervalId);
+  clearInterval(clockUpdateInterval);
 }
 
+function resetClock(clock) {
+
+  clearInterval(clockUpdateInterval);
+  clock = {};
+}
 function stopPomodoro() {
   state = null;
   resetClock();
 }
-function startPomodoro() {
-  state = 'running';
-  const time = 25;
-  const clock = {
-    minutes: time,
-    separator: ':',
-    seconds: 0
-  }
 
-  intervalId = setInterval(updateClock, 1000);
-  function decreaseMinute(clock) {
+function start(clock) {
+  function getTime(clock) {
+    return clock.minutes * 60 * 1000 + clock.seconds * 1000;
+  }
+  const totalTime = getTime(clock);
+  clockUpdateInterval = setInterval(updateClock, 1000);
+  const refreshRate = 100;
+  progressBarUpdateInterval = setInterval(updateProgressBar, refreshRate);
+  let timeElapsed = 0; 
+  function updateProgressBar() {
+    timeElapsed += refreshRate;
+    const currentTimeRemaining = totalTime - timeElapsed; 
+    const progressBarWidth = `${(totalTime - currentTimeRemaining) / totalTime * 100}%`;
+    progressBar.style.width = progressBarWidth;
+  }
+  function decreaseMinute() {
     clock.minutes--;
     clock.seconds = 59;
   }
@@ -36,21 +64,63 @@ function startPomodoro() {
   function updateClock() {
     clock.seconds--;
     if (clock.seconds < 0) {
-      decreaseMinute(clock);
+      decreaseMinute();
     }
     console.log(showClock(clock));
     if (timerIsUp(clock)) {
-      clearInterval(intervalId);
+      clearInterval(clockUpdateInterval);
+      progressBar.style.width = '0px';
+      clearInterval(progressBarUpdateInterval);
+      if (clock.type === 'focus') {
+        updateSessions(++sessions);
+        return;
+      }
+      console.log('Break is over');
     }
   }
 }
 
+function startPomodoro() {
+  state = 'running';
+  const time = 1;
+  const clock = {
+    minutes: time,
+    separator: ':',
+    seconds: 0,
+    type: 'focus'
+  }
+  start(clock);
+
+}
+
+function startBreak() {
+  const clock = {
+    minutes: 5,
+    separator: ':',
+    seconds: 0,
+    type: 'break'
+  }
+  start(clock);
+}
+
+function startLongBreak() {
+  const clock = {
+    minutes: 15,
+    separator: ':',
+    seconds: 0,
+    type: 'break'
+  }
+  start(clock);
+}
 
 
-const clockHeading = document.querySelector('.clock');
+const clocks = [...document.querySelectorAll('.clock')];
 const controls = document.querySelector('.controls');
-let intervalId = null;
+const session = document.querySelector('.sessions p');
+const progressBar = document.querySelector('.progress-bar');
+let clockUpdateInterval = null;
 let state = null;
+let sessions = 3;
 controls.addEventListener('click', event => {
   if (event.target.className === 'start') {
     if (state !== 'running') {
